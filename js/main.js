903f0d50d9fae5b3204dc53a59bb8fadd48244ce
chrislcassidy <chrislcassidy@gmail.com>
2017-12-02 20:54:11 -0500

    New Java Project

--- js/main.js
+++ js/main.js
@@ -4,30 +4,34 @@ var currentEncounter = [];
 var savedEncounter = [];
 $(document).ready(function(){
 	
+	$('.tabs').tabs();
+	
 	pullCharacters();
 	pullEncounters();
 	
-	$('#navChar').off().on('click',function(){
-		handleNavClick($(this));
-		charactersView();
+	$('#characters').off().on('click',function(){
+		pullCharacters();
 	});
+	
+	$('.controlgroup').controlgroup();
+	$('input[name="entityType"]').checkboxradio({
+    	icon: false
+    });
+	
 });
 
 function charactersView() {
-	$('#content').append('<div class="characterList"></div>');
+	$('#charactersTab').html('');
 	for(i=0;i<characters.length;++i){
 		var c = characters[i];
-		$('.characterList').append(characterDiv.format(c.name,c.type,c.class,c.background,c.player,c.alignment,
+		$('#charactersTab').append(characterDiv.format(c.name,c.type,c.class,c.background,c.player,c.alignment,
 			c.str,c.dex,c.con,c.int,c.wis,c.cha,c.level,
 			calculateBonus(c.str),calculateBonus(c.dex),calculateBonus(c.con),calculateBonus(c.int),calculateBonus(c.wis),calculateBonus(c.cha),
 			calculateBonus(c.dex),i,i)
 		);
 	}
 	
-	$('.name').off().on('click',function(){
-		var id = $(this).attr('data-id');
-		$('.characterInner[data-id="'+id+'"]').slideToggle();	
-	});	
+	$( ".accordion" ).accordion({active: false,collapsible: true});
 } 
 
 function characterEdit(index) {
@@ -41,21 +45,17 @@ function calculateBonus(baseStat) {
 	return statBonuses[baseStat];
 }
 
-function handleNavClick(btn) {
-	$('#content').html('');
-	$('.navBtn').css('background-color','burlywood');
-	btn.css('background-color','palegoldenrod');
-}
-
 function pullCharacters() {
 	$.ajax({
     	type: "GET" ,
     	url: "xml/characters.xml" ,
     	dataType: "xml" ,
     	success: function(xml) {
+    		characters = [];
 	    	$(xml).find('character').each(function(){
      			characters.push(new Character($(this)));  
     		}); 
+    		charactersView();
     	}       
 	});
 }
@@ -75,8 +75,8 @@ function pullEncounters() {
 
 statBonuses={'1':'-5','2':'-4','3':'-4','4':'-3','5':'-3','6':'-2','7':'-2','8':'-1','9':'-1','10':'0','11':'0','12':'+1','13':'+1','14':'+2','15':'+2','16':'+3','17':'+3','18':'+4','19':'+4','20':'+5','21':'+5','22':'+6','23':'+6','24':'+7','25':'+7','26':'+8','27':'+8','28':'+9','29':'+9','30':'+10'};
 
-characterDiv='<div class="character">'+
-				'<div class="name" data-id="{20}">{0} <span class="editBtn">Edit</span></div>'+
+characterDiv='<div class="character accordion">'+
+				'<h3 class="name" data-id="{20}">{0}</h3>'+
 				'<div class="characterInner" style="display:none;" data-id="{21}">'+
 					'<div class="basic">'+
 						'<div class="basicInner">Type<div>{1}</div></div>'+
